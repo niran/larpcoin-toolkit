17bb22ec1a044d074766ff5fadc33547c93807d5
Niran Babalola <niran@niran.org>
2024-03-25 03:00:34 +0000

    Layout the $CRAFT landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["forest"],
+  }
 };
 export default config;
