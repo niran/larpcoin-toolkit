8b4380e4752f5f65c35b9ad07a910c706e87b39e
Niran Babalola <niran@niran.org>
2024-03-25 23:34:13 +0000

    Attempt to exclude the evm folder from next builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  exclude: ["evm"],
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
